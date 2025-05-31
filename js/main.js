5cd74ce18956dbeb570f57e60f306184f4a304d2
YanKarpov <KarpovYAA23@spb.ithub.ru>
2025-05-31 15:59:16 +0300

    расписал параллакс для звёзд

--- js/main.js
+++ js/main.js
@@ -12,49 +12,46 @@ resize();
 window.addEventListener('resize', resize);
 
 class Star {
-  constructor() {
+  constructor(layer = 1) {
+    this.layer = layer;
     this.reset();
   }
   reset() {
     this.x = Math.random() * W;
     this.y = Math.random() * H;
-    this.radius = Math.random() * 1.3 + 0.3;
-    this.alpha = Math.random();
-    this.alphaSpeed = 0.005 + Math.random() * 0.02;
+    this.radius = (Math.random() * 1.3 + 0.3) / this.layer;
+    this.alpha = Math.random() * 0.5 + 0.5;
+    this.alphaSpeed = 0.002 + Math.random() * 0.01;
     this.alphaDirection = Math.random() < 0.5 ? 1 : -1;
   }
   update() {
     this.alpha += this.alphaSpeed * this.alphaDirection;
-    if (this.alpha <= 0) {
-      this.alpha = 0;
-      this.alphaDirection = 1;
-    }
-    if (this.alpha >= 1) {
-      this.alpha = 1;
-      this.alphaDirection = -1;
+    if (this.alpha <= 0 || this.alpha >= 1) {
+      this.alphaDirection *= -1;
     }
   }
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
-    ctx.shadowColor = `rgba(255, 255, 255, ${this.alpha})`;
-    ctx.shadowBlur = 6;
+    ctx.fillStyle = `rgba(255,255,255,${this.alpha})`;
+    ctx.shadowColor = `rgba(255,255,255,${this.alpha})`;
+    ctx.shadowBlur = 6 / this.layer;
     ctx.fill();
     ctx.closePath();
   }
 }
 
 const stars = [];
-const starCount = 200;
-for (let i = 0; i < starCount; i++) {
-  stars.push(new Star());
-}
+[100, 120, 140].forEach((count, layer) => {
+  for (let i = 0; i < count; i++) {
+    stars.push(new Star(layer + 1));
+  }
+});
 
 function animate() {
   ctx.clearRect(0, 0, W, H);
 
-  let gradient = ctx.createLinearGradient(0, 0, 0, H);
+  const gradient = ctx.createLinearGradient(0, 0, 0, H);
   gradient.addColorStop(0, '#000022');
   gradient.addColorStop(1, '#000010');
   ctx.fillStyle = gradient;
